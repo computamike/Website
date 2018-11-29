e685478ab09e396b95155fe91cb1be3295f15140
Mike Hingley <computa_mike@hotmail.com>
2018-11-29 09:00:27 +0000

    Refactored Classes to allow better serialization.

--- CLI/showmaker_new.php
+++ CLI/showmaker_new.php
@@ -42,192 +42,6 @@ class Utility{
     }
 }
 
-
-
-// THIS IS THE CODE THAT WAS RUNNING THE DAILY SHOW
-// if (isset($json_data['daily_show']) && $daily) {
-//     $show_data = $json_data['daily_show'];
-//     echo "Creating Daily Show..." . PHP_EOL;
-//     echo sprintf('The Daily track is %1$s by %2$s ' . PHP_EOL . "", $show_data['arrTracks'][1]['strTrackName'], $show_data['arrTracks'][1]['strArtistName']);
-//     debugout::add("Making intro bumper");
-//     $running_order = addEntryToJsonArray('', 0, 'intro');
-//     if ( ! generateSilenceWav(7, Configuration::getWorkingDir() . '/pre-show-silence.wav')) {
-//         debugout::dump("WARNING: Failed to create silence.");
-//     }
-//     $intro = "$pre_sable" . PHP_EOL;
-//     $intro .= sprintf(
-//         randomTextSelect(
-//             array(
-//                 'Hello and welcome to the %1$s from %2$s <BREAK LEVEL="MEDIUM" /> To dayz show features ',
-//                 'Hey there <BREAK LEVEL="MEDIUM" /> You are listening to a feed from %2$s and this is the %1$s <BREAK LEVEL="MEDIUM" /> To day you can hear '
-//             )
-//         ),
-//         $show_data['strShowNameSpoken'], 
-//         $show_data['strSiteNameSpoken']
-//     );
-//     if ($show_data['isNSFW'] != 0) {
-//         $intro .= randomTextSelect($track_nsfw);
-//     }
-//     $intro .= sprintf(
-//         randomTextSelect(
-//             array(
-//                  '%1$s <BREAK LEVEL="SMALL" /> by <BREAK LEVEL="SMALL" /> %2$s',
-//                 ' a track by %2$s <BREAK LEVEL="SMALL" /> called <BREAK LEVEL="SMALL" /> %1$s'
-//             )
-//         ),
-//         preg_replace('/\&/', ' and ', $show_data['arrTracks'][1]['strTrackNameSounds']),
-//         preg_replace('/\&/', ' and ', $show_data['arrTracks'][1]['strArtistNameSounds'])
-//     );
-//     $intro .= PHP_EOL . "$post_sable";
-//     if ( ! convertSableXmlToWav($intro, Configuration::getWorkingDir() . '/intro.wav')) {
-//         debugout::dump("WARNING: Failed to create intro using $intro");
-//         die('Thats it... im done');
-//     }
-//     if ( ! concatenateTracks(Configuration::getWorkingDir() . '/pre-show-silence.wav', Configuration::getWorkingDir() . '/intro.wav', Configuration::getWorkingDir() . '/showstart.wav')) {
-//         debugout::dump("WARNING: Failed to concatenate pre-show-silence with intro.wav");
-//     }
-//     copy(Configuration::getStaticDir() . '/intro.wav', Configuration::getWorkingDir() . '/intro.wav');
-//     if ( ! overlayAudioTracks(Configuration::getWorkingDir() . '/showstart.wav', Configuration::getWorkingDir() . '/intro.wav', Configuration::getWorkingDir() . '/run.wav')) {
-//         debugout::dump("WARNING: Failed to overlay showstart.wav over intro.wav");
-//     }
-//     $arrTracks[$show_data['arrTracks'][1]['intTrackID']] = $show_data['arrTracks'][1];
-//     $running_order = addEntryToJsonArray($running_order, getTrackLength(Configuration::getWorkingDir() . '/run.wav'), $show_data['arrTracks'][1]['intTrackID']);
-//     debugout::add("Downloading and merging audio file");
-//     $track = downloadFile($show_data['arrTracks'][1]['localSource']);
-//     if ($track === false) {
-//         debugUnlink(Configuration::getWorkingDir() . '/run.wav');
-//         debugout::dump();
-//         die("The track is not currently available." . PHP_EOL);
-//     }
-//     copy($track, Configuration::getWorkingDir() . '/' . $show_data['arrTracks'][1]['fileSource']);
-//     debugUnlink($track);
-//     if ( ! trackTrimSilence(Configuration::getWorkingDir() . '/' . $show_data['arrTracks'][1]['fileSource'])) {
-//         debugout::dump("WARNING: Failed to trim the silence from {$show_data['arrTracks'][1]['fileSource']}");
-//     }
-//     debugUnlink(Configuration::getWorkingDir() . '/' . $show_data['arrTracks'][1]['fileSource']);
-//     if ( ! concatenateTracks(Configuration::getWorkingDir() . '/run.wav', Configuration::getWorkingDir() . '/' . $show_data['arrTracks'][1]['fileSource'] . '.trim.wav', Configuration::getWorkingDir() . '/runplustrack.wav')) {
-//         debugout::dump("WARNING: Failed to concatenate run.wav with {$show_data['arrTracks'][1]['fileSource']}");
-//     }
-//     $running_order = addEntryToJsonArray($running_order, getTrackLength(Configuration::getWorkingDir() . '/runplustrack.wav'), 'outro');
-//     $outro = "$pre_sable" . PHP_EOL . "<BREAK LEVEL=\"LARGE\" />";
-//     $outro .= sprintf(
-//         randomTextSelect(
-//             array(
-//                 'That was <BREAK LEVEL="SMALL" /> %1$s <BREAK LEVEL="SMALL" /> by <BREAK LEVEL="SMALL" /> %2$s <BREAK LEVEL="MEDIUM" /> It was a %3$s licensed track',
-//                 'You were listening to a %3$s licensed track by %2$s <BREAK LEVEL="SMALL" /> called <BREAK LEVEL="SMALL" /> %1$s'
-//             )
-//         ),
-//         preg_replace('/\&/', ' and ', $show_data['arrTracks'][1]['strTrackNameSounds']),
-//         preg_replace('/\&/', ' and ', $show_data['arrTracks'][1]['strArtistNameSounds']),
-//         preg_replace('/\&/', ' and ', $show_data['arrTracks'][1]['pronouncable_enumTrackLicense'])
-//     );
-//     $outro .= sprintf(
-//         randomTextSelect(
-//             array(
-//                 ' <BREAK LEVEL="MEDIUM" /> Every track we play is selected by a listener like you <BREAK LEVEL="LARGE" /> to find out more <BREAK LEVEL="SMALL" /> please visit %1$s slash <BREAK LEVEL="MEDIUM" /> eff <BREAK LEVEL="SMALL" /> ay <BREAK LEVEL="SMALL" /> queue <BREAK LEVEL="LARGE" /> If you liked to dayz track, you can vote for it at %2$s <BREAK LEVEL="MEDIUM" /> These votes decide whether this track will be on the weekly show and eventually if it will make it into the chart <BREAK LEVEL="MEDIUM" /> both of these can be found by visiting %1$s ',
-//                 ' <BREAK LEVEL="MEDIUM" /> Remember, you can vote for this track by visiting %2$s <BREAK LEVEL="MEDIUM" /> Your vote will decide whether it makes it into the best-of-the-week <BREAK LEVEL="SMALL" /> weekly show which is available from %1$s slash weekly '
-//             )
-//         ), 
-//         $show_data['strSiteNameSpoken'],
-//         $show_data['strShowUrlSpoken']
-//     );
-//     $outro .= sprintf(' <BREAK LEVEL="LARGE" /> The theem is an exerpt from Gee Em Zed By Scott All-tim <BREAK LEVEL="SMALL" />for details, please visit %1$s slash theem', $show_data['strSiteNameSpoken']) . PHP_EOL . $post_sable;
-//     debugout::add("Making the outro bumper");
-//     if ( ! convertSableXmlToWav($outro, Configuration::getWorkingDir() . '/outro.wav')) {
-//         debugout::dump("WARNING: Failed to generate the sable file or create outro.wav");
-//     }
-//     if ( ! generateSilenceWav(34, Configuration::getWorkingDir() . '/post-show-silence.wav')) {
-//         debugout::dump("WARNING: Failed to create silence.");
-//     }
-//     if ( ! concatenateTracks(Configuration::getWorkingDir() . '/outro.wav', Configuration::getWorkingDir() . '/post-show-silence.wav', Configuration::getWorkingDir() . '/showend.wav')) {
-//         debugout::dump("WARNING: Failed to concatenate outro.wav with post-show-silence.wav");
-//     }
-//     if ( ! reverseTrackAudio(Configuration::getWorkingDir() . '/showend.wav', Configuration::getWorkingDir() . '/showend_rev.wav')) {
-//         debugout::dump("WARNING: Failed to reverse showend.wav into showend_rev.wav.");
-//     }
-//     if ( ! reverseTrackAudio(Configuration::getStaticDir() . '/outro.wav', Configuration::getWorkingDir() . '/outro_rev.wav', false)) {
-//         debugout::dump("WARNING: Failed to reverse outro.wav into outro_rev.wav");
-//     }
-//     if ( ! overlayAudioTracks(Configuration::getWorkingDir() . '/showend_rev.wav', Configuration::getWorkingDir() . '/outro_rev.wav', Configuration::getWorkingDir() . '/run_rev.wav')) {
-//         debugout::dump("WARNING: Failed to overlay showend_rev.wav with outro_rev.wav");
-//     }
-//     if ( ! reverseTrackAudio(Configuration::getWorkingDir() . '/run_rev.wav', Configuration::getWorkingDir() . '/run.wav')) {
-//         debugout::dump("WARNING: Failed to reverse run_rev.wav into run.wav");
-//     }
-//     if ( ! concatenateTracks(Configuration::getWorkingDir() . '/runplustrack.wav', Configuration::getWorkingDir() . '/run.wav', Configuration::getWorkingDir() . '/daily.wav')) {
-//         debugout::dump("WARNING: Failed to concatenate runplustrack.wav with run.wav");
-//     }
-//     $running_order = addEntryToJsonArray($running_order, getTrackLength(Configuration::getWorkingDir() . '/daily.wav'), 'end');
-//     $arrRunningOrder = makeArrayFromObjects(json_decode($running_order));
-//     foreach ($arrRunningOrder as $timestamp => $entry) {
-//         if (0 + $entry > 0) {
-//             $arrRunningOrder_final[(string) $timestamp] = $arrTracks[$entry];
-//         } else {
-//             $arrRunningOrder_final[(string) $timestamp] = $entry;
-//         }
-//     }
-//     debugout::add("Getting the coverart");
-//     $coverart = downloadFile($show_data['qrcode']);
-//     if ($coverart != false) {
-//         copy($coverart, Configuration::getWorkingDir() . '/' . $show_data['intShowID'] . '.png');
-//         debugUnlink($coverart);
-//         $coverart = Configuration::getWorkingDir() . '/' . $show_data['intShowID'] . '.png';
-//     } else {
-//         $coverart = '';
-//     }
-//     debugout::add("Converting the daily show to the various formats");
-//     WaitForSomeKeyboardDuty();
-//     generateOutputTracks(
-//         Configuration::getWorkingDir() . '/daily.wav',
-//         Configuration::getWorkingDir() . '/daily.' . $show_data['intShowUrl'] . '.',
-//         array(
-//             'Title' => $show_data['strShowName'],
-//             'Artist' => 'CCHits.net',
-//             'AlbumArt' => $coverart,
-//             'RunningOrder' => $arrRunningOrder_final
-//         )
-//     );
-//     if ($coverart != '') {
-//         debugUnlink($coverart);
-//     }
-//     finalize(
-//         $show_data['intShowID'],
-//         Configuration::getWorkingDir() . '/daily.' . $show_data['intShowUrl'] . '.', 
-//         updateStatusNet(
-//             array(
-//                 sprintf(
-//                     randomTextSelect(
-//                         array(
-//                             'A new !daily show has been created for %1$s-%2$s-%3$s. Get it from %4$s'
-//                         )
-//                     ), 
-//                     substr($show_data['intShowUrl'], 0, 4),
-//                     substr($show_data['intShowUrl'], 4, 2),
-//                     substr($show_data['intShowUrl'], 6, 2),
-//                     $show_data['shorturl']
-//                 ),
-//                 sprintf(
-//                     randomTextSelect(
-//                         array(
-//                             'The @%1$s daily show for today (%2$s) features %3$s by %4$s'
-//                         )
-//                     ),
-//                     Configuration::getStatusNetUser(),
-//                     $show_data['shorturl'],
-//                     $show_data['arrTracks'][1]['strTrackName'],
-//                     $show_data['arrTracks'][1]['strArtistName']
-//                 )
-//             )
-//         ),
-//         json_encode($arrRunningOrder_final),
-//         'daily',
-//         $show_data['intShowUrl']
-//     );
-//     echo "Done." . PHP_EOL . PHP_EOL;
-//     debugout::reset();
-// }
-///
-
 /**
  * Information about Tracks (this may move back as it probably already exists)
  */
@@ -236,11 +50,24 @@ class showTrack{
     public $TrackName;
     public $Artist;
     public $TrackURL;
-
+    public function process(){
+        echo(' 🎹  DJ PLAY : ' . $this->TrackName . PHP_EOL );
+        $this->duration=rand(60,120);
+    }
+}
+//implements JsonSerializable
+class showSpeech {
+    public function __construct($script,$description){
+        $this->script = $script;
+        $this->description = $description;
+    }
+    public function process(){
+        echo(' 👄  DJ SAY : ' . $this->script . PHP_EOL );
+        $this->duration=rand(10,20);
+    }
 }
 
-class DailyShow{
-    // echo sprintf('The Daily track is %1$s by %2$s ' . PHP_EOL . "", $show_data['arrTracks'][1]['strTrackName'], $show_data['arrTracks'][1]['strArtistName']);
+class DailyShow {
     private $showName = "Daily Show";
     private $siteName = 'CCHits';
     private $introScript = [
@@ -249,10 +76,32 @@ class DailyShow{
 
     private $available_Intro_text =  'INTRO: The @%1$s daily show for today (%2$s) features %3$s by %4$s';
     private $track_list = [];
-    private $outro_text = 'OUTTRO: You have been listening to CChits - ';
+    private $outro_text = 'OUTRO: You have been listening to CChits - ';
+    
+    /**
+     * Add a Show element (voice over, track...)
+     *
+     * @param [type] $TimeOffset
+     * @param [type] $object
+     * @return void
+     */
+    private function addShowElement($TimeOffset,$object){
+        array_push ($this->track_list, array($TimeOffset => $object));
+    }
 
     public function __construct(){
-        $this->available_Intro_text = sprintf(Utility::randomTextSelect($this->introScript),$this->showName, $this->siteName);
+        $this->available_Intro_text = sprintf(Utility::randomTextSelect($this->introScript),$this->showName, $this->siteName);  
+        $intro = new showSpeech($this->available_Intro_text,'description');
+        $this->addShowElement("1",$intro);
+        // This track object would be returned from the database usually
+        $Track1 = new showTrack();
+        $Track1->TrackName = 'This is My Jam';
+        $Track1->TrackID = 100;
+        $Track1->Artist = 'Jam Master 5000';
+        $Track1->TrackURL = 'http://foobar';
+        $this->addShowElement("15",$Track1);
+        $this->addShowElement("40",new showSpeech($this->outro_text,'description'));
+        
     }
 
     // 2. Create this method and add your desired read-only public property.
@@ -264,25 +113,36 @@ class DailyShow{
  
     public function add_track($track){
         echo('✨ Adding a Track :' . $track->TrackName .PHP_EOL);
-        array_push ( $this->track_list,$track);
+        //$this->addShowElement("19.83",$track);
+        //array_push ( $this->track_list,$track);
     }
 
     public function process_show(){
         echo('✨ Processing Show'.PHP_EOL);
-        echo(' 🎹  DJ PLAY : CChits Intro' . PHP_EOL );
-        echo(' 👄  DJ SAY : ' . $this->name . PHP_EOL );
-        echo(' 👄  DJ SAY2 : ' . $this->available_Intro_text. PHP_EOL );
-        
-        // var_dump($this);
+        //echo(' 🎹  DJ PLAY : CChits Intro' . PHP_EOL );
+        //echo(' 👄  DJ SAY : ' . $this->name . PHP_EOL );
+        //echo(' 👄  DJ SAY2 : ' . $this->available_Intro_text. PHP_EOL );
+        $updatedtrack_list = [];
+        $TimeOffset = 0;
         foreach ($this->track_list  as $track)
         {
-            echo(' 👄  DJ SAY  : ' . $track->TrackName . PHP_EOL );
-            echo(' 🎹  DJ PLAY : ' . $track->TrackURL . PHP_EOL );
-            echo(' 👄  DJ SAY  : That was ' . $track->TrackName . ' by ' . $track->Artist  .  PHP_EOL );
+            $element = reset($track);
+            $element->process();
+            array_push ($updatedtrack_list, array(strval($TimeOffset) => $element));
+            $TimeOffset  = $TimeOffset  + $element->duration ;
         }
-        echo(' 👄  DJ SAY : ' . $this->outro_text . PHP_EOL );
+        $this->track_list = $updatedtrack_list;
         echo(' ✨  Convert to MP3, OGG and MP4');
     }
+
+    public function jsonSerialize() {
+        return $this->track_list;
+    }
+
+    public function toJSON(){
+        return json_encode($this->track_list, JSON_PRETTY_PRINT);
+    }
+
 }
 
 /**
@@ -301,27 +161,27 @@ class ShowMaker{
 }
 
 
+
 $sm = new ShowMaker();
 
 // This should eventually provide a weekly monthly or daily object...
 $dailyShow = $sm->MakeShow();
 
 // Let's create a track : - this should be 
-$Track1 = new showTrack();
-$Track1->TrackName = 'This is My Jam';
-$Track1->TrackID = 100;
-$Track1->Artist = 'Jam Master 5000';
-$Track1->TrackURL = 'http://foobar';
+
 
 // so we've set up our show
-$dailyShow->add_track($Track1);
+// $dailyShow->add_track($Track1);
 // var_dump($dailyShow);
 // Now we tell it process...
 $dailyShow->process_show();
 
-$s = serialize($dailyShow);
+$t = $dailyShow->toJSON();
+echo($t);
 
-echo('serialized DAILY SHOW'.PHP_EOL);
-print_r($s);
-
+// echo('serialized DAILY SHOW'.PHP_EOL);
+// echo('   '.PHP_EOL);
+// echo('   '.PHP_EOL);
+// echo str_replace('\"','"', $s);
+// echo('   '.PHP_EOL);
 // All Done.
